49e4c896c66b4f78ae69b29d971820fd9b20ef5e
CraigSau <37848439+CraigSau@users.noreply.github.com>
2021-11-28 15:18:10 -0500

    Test Fix

--- Website/JavaScript/server.js
+++ Website/JavaScript/server.js
@@ -1,12 +1,12 @@
 
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
 
-// app.get("/", function (req, res) {
-//     res.sendFile("C:/Users/craig/Documents/GitHub/CraigSau.github.io/index.html");
-//     res.sendFile("C:/Users/craig/Documents/GitHub/CraigSau.github.io/CSS/base-style.css");
-// });
+app.get("/", function (req, res) {
+    res.sendFile("C:/Users/craig/Documents/GitHub/CraigSau.github.io/index.html");
+    // res.sendFile("C:/Users/craig/Documents/GitHub/CraigSau.github.io/CSS/base-style.css");
+});
 
 app.get("/contact", function (req, res) {
     res.send("Contact me at: craigsauers@ymail.com");
