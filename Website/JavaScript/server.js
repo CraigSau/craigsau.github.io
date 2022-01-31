6dbeafcebcce9613e0c1d90500419cc58d659d14
CraigSau <37848439+CraigSau@users.noreply.github.com>
2022-01-31 08:15:38 -0500

    fixed untracked files

--- Website/JavaScript/server.js
+++ Website/JavaScript/server.js
@@ -1,19 +1,18 @@
 
 const express = require("express");
+const path = require("path");
+const fileupload = require('express-fileupload');
+
+let initial_path = path.join(__dirname, 'public');
+
 const app = express();
-// app.use(express.static(__dirname));
+app.use(express.static(initial_path));
+app.use(fileupload());
 
-app.get("/", function (req, res) {
-    res.sendFile("C:/Users/craig/Documents/GitHub/CraigSau.github.io/index.html");
-    // res.sendFile("C:/Users/craig/Documents/GitHub/CraigSau.github.io/CSS/base-style.css");
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, "blog.html"));
 });
 
-app.get("/contact", function (req, res) {
-    res.send("Contact me at: craigsauers@ymail.com");
+app.listen('3000', () => {
+    console.log('listening.....');
 });
-
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
-});
-
- 
